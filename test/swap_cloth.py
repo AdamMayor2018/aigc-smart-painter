eb40bf69e2aed2761f7dc881f568502a94f3b8bd
caoxiang <caoxiang@yangshipin.cn>
2023-07-12 19:36:11 +0800

    增加多重controlnet的inpaint功能支持

--- test/swap_cloth.py
+++ test/swap_cloth.py
@@ -28,12 +28,13 @@ from service.magic_closet import MagicCloset
 #((1 girl)), (goddess-like happiness:1.2), Kpop idol, smile, (RAW photo:1.2), (photorealistic:1.4), (masterpiece:1.3), (intricate details:1.2), delicate, beautiful detailed, (detailed eyes), (detailed facial features), (20 years old female with fashion clothes), tall female, petite, small breasts, narrow waist, (facing camera), (looking_at_viewer:1.3), from_front, big circle earrings, slim_legs, (best quality:1.4), (ultra highres:1.2), cinema light, outdoors, (extreme detailed illustration), (lipgloss, eyelashes, best quality, ultra highres, depth of field, caustics, Broad lighting, natural shading, 85mm, f/1.4, ISO 200, 1/160s:0.75)
 
 # 读取图片
-img_path = "/data/cx/ysp/aigc-smart-painter/assets2/cloth1.jpg"
+img_path = "/data/cx/ysp/aigc-smart-painter/assets2/dress1.jpg"
 image = Image.open(img_path)
 conf_loader = YamlConfigLoader(yaml_path="/data/cx/ysp/aigc-smart-painter/config/general_config.yaml")
 # 初始化服务
 cloth_service = MagicCloset(conf_loader)
-prompt = "1 girl,(portrait:1.2),model shoot style, smooth facial skin, black hair, indoors,Hands akimbo,carrying a women ba"
+# prompt = "1 girl,(portrait:1.2),model shoot style, black hair, intricate details, (real skin texture:1.2), indoors,both hands in pockets, ultra realistic,Fujifilm XT3,film grain,8k,uhd"
+prompt = "1 girl,(portrait:1.2),model shoot style, black hair, intricate details, (real skin texture:1.2), , ultra realistic,Fujifilm XT3,film grain,8k,uhd,hands akimbo"
 mask_image, control_images, images = cloth_service.swap_model(image, part="upper-clothes", prompt=prompt, num_images_per_prompt=8, num_inference_steps=50, strength=0.6)
 #mask_image,control_images, images = cloth_service.swap_background(image, prompt, num_images_per_prompt=8,reverse=True)
 plt.imsave(f"/data/cx/ysp/aigc-smart-painter/assets2/mask.jpg", np.array(mask_image))
